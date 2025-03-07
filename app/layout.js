a36bd5150add8ec9e7708a8661b2a9f22160c885
dan10ish <aansaridan@gmail.com>
2025-03-07 08:19:51 +0300

    Major Fix

--- app/layout.js
+++ app/layout.js
@@ -9,18 +9,13 @@ import "./Styles.css";
 import "./Window.css";
 
 export const metadata = {
-  title: "Sonoma",
-//   description: "After creating [FigCV](https://read.cv/sites/templates/f05h8yxhy), [Julius Tarng](https://read.cv/tarngerine) had a great idea for a template based on MacOS but didn't have the time to build it. So I tried to do the idea justice and came up with this.
+  title: "Danish",
+  description: "Danish's personal site.",
+  metadataBase: new URL('https://os.danish.bio'),
+};
 
-// Sonoma features a fun and familiar presentation of your work and experiences, while also playfully supporting your status updates and a custom music player.
-
-// Sonoma is made to be highly customizable, just like your real desktop. We have a [video tutorial on customization available here](https://www.youtube.com/watch?v=QJPQrMIYOXk).
-
-// To change your background image, open the Media Manager and scroll to "backdrop.jpg". Hit the three dot button and click replace to upload your own background image.
-
-// To change the song and artwork behind the "Listen" app, replace "soundtrack.mp3" and "soundtrack-cover.jpg" in the media manager.
-
-// Lastly, to change the appearance of the toolbar or music player, open SiteSettings in the editor and change the values from “light” to “dark”.",
+export const viewport = {
+  themeColor: "#1c01a0",
 };
 
 export default function RootLayout({ children }) {
--- app/page.js
+++ app/page.js
@@ -181,6 +181,20 @@ function Desktop(props) {
             }}
           />
         : null}
+        <a href="https://danish.bio" target="_blank" style={{ textDecoration: 'none' }}>
+          <Icon
+            icon={
+              <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none" stroke="#0066CC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style={{ position: 'absolute', inset: 0 }}>
+                <circle cx="12" cy="12" r="10"/>
+                <path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/>
+                <path d="M2 12h20"/>
+              </svg>
+            }
+            collection={{
+              name: "Website"
+            }}
+          />
+        </a>
       </div>
       {windows.length > 0 ?
         <div className="windows">
